cf12e28fc2e5dff2da27e833fab37a5337a6de5e
Mārcis <60743431+MarcisAn@users.noreply.github.com>
2025-11-08 13:55:33 +0200

    commnet out old code in visualiser

--- visualiser/src/main.ts
+++ visualiser/src/main.ts
@@ -47,49 +47,49 @@ socket.on("animationData", (data) => {
   already_running = true;
 });
 
-socket.on("reciveAnimation", (data) => {
-  clearInterval(intervalId);
-  if (record) {
-    alert("RECORD START");
-    recording = true;
-    recorder.start();
-  }
-  anim_speed = data.speed;
-  frame = 0;
-  all_to_black();
-  anim_data = [];
-  data.animation.forEach((frame: string) => {
-    let tree_data: number[][] = [];
-    let light_data: number[] = [];
-    let char_index = 0; // indekss baitam, kuru pašlaik apskatām: 0-lampiņas idekss, 1-red, 2-green, 3-blue
-    frame.split("").forEach(function (c: string) {
-      light_data.push(c.charCodeAt(0));
-      char_index++;
-      if (char_index == 4) {
-        char_index = 0;
-        tree_data.push(light_data);
-        light_data = [];
-      }
-    });
-    anim_data.push(tree_data);
-  });
-  intervalId = window.setInterval(function () {
-    if (anim_data.length > 0) {
-      renderVis(anim_data[frame]);
-    }
-    frame++;
+// socket.on("reciveAnimation", (data) => {
+//   clearInterval(intervalId);
+//   if (record) {
+//     alert("RECORD START");
+//     recording = true;
+//     recorder.start();
+//   }
+//   anim_speed = data.speed;
+//   frame = 0;
+//   all_to_black();
+//   anim_data = [];
+//   data.animation.forEach((frame: string) => {
+//     let tree_data: number[][] = [];
+//     let light_data: number[] = [];
+//     let char_index = 0; // indekss baitam, kuru pašlaik apskatām: 0-lampiņas idekss, 1-red, 2-green, 3-blue
+//     frame.split("").forEach(function (c: string) {
+//       light_data.push(c.charCodeAt(0));
+//       char_index++;
+//       if (char_index == 4) {
+//         char_index = 0;
+//         tree_data.push(light_data);
+//         light_data = [];
+//       }
+//     });
+//     anim_data.push(tree_data);
+//   });
+//   intervalId = window.setInterval(function () {
+//     if (anim_data.length > 0) {
+//       renderVis(anim_data[frame]);
+//     }
+//     frame++;
 
-    if (frame == anim_data.length) {
-      if (record && recording) {
-        recorder.stop();
-        recorder.save();
-        record = false;
-      }
-      frame = 0;
-    }
-    //console.log(Math.pow(anim_speed, -1)*400)
-  }, Math.pow(anim_speed, -1) * 400);
-});
+//     if (frame == anim_data.length) {
+//       if (record && recording) {
+//         recorder.stop();
+//         recorder.save();
+//         record = false;
+//       }
+//       frame = 0;
+//     }
+//     //console.log(Math.pow(anim_speed, -1)*400)
+//   }, Math.pow(anim_speed, -1) * 400);
+// });
 
 let html = ``;
 if (process.env.NODE_ENV == "development") {
